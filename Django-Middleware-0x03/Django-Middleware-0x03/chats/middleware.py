c2ba92d4a188ee03b874bb7def6f8306e1489d56
hichamtalha6 <hichamtalha6@gmail.com>
2025-10-05 17:22:43 +0100

    Update middleware.py

--- Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/chats/middleware.py
@@ -1,10 +1,34 @@
 from datetime import datetime
 from django.http import HttpResponseForbidden
+import logging
+
+
+class RequestLoggingMiddleware:
+    """Logs each user’s requests to a file with timestamp, user, and path."""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # Configure logger
+        self.logger = logging.getLogger(__name__)
+        handler = logging.FileHandler("requests.log")
+        formatter = logging.Formatter('%(message)s')
+        handler.setFormatter(formatter)
+        if not self.logger.handlers:
+            self.logger.addHandler(handler)
+        self.logger.setLevel(logging.INFO)
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        self.logger.info(log_message)
+        response = self.get_response(request)
+        return response
+
 
 class RestrictAccessByTimeMiddleware:
     """
-    Middleware to restrict access to the site between certain hours.
-    Allows access only between 6:00 PM (18:00) and 9:00 PM (21:00).
+    Middleware to restrict access outside allowed hours.
+    Allows access only between 6 PM (18:00) and 9 PM (21:00).
     """
 
     def __init__(self, get_response):
@@ -19,6 +43,5 @@ class RestrictAccessByTimeMiddleware:
                 "<h1>403 Forbidden</h1><p>Access to the chat is only allowed between 6 PM and 9 PM.</p>"
             )
 
-        # Continue normally during allowed hours
         response = self.get_response(request)
         return response

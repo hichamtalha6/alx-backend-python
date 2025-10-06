2a7dd7c0e01b58e1e7f144f63b06755ffa36a2fe
hichamtalha6 <hichamtalha6@gmail.com>
2025-10-06 20:20:04 +0100

    Update apps.py

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -6,4 +6,4 @@ class MessagingConfig(AppConfig):
     name = 'messaging'
 
     def ready(self):
-        import messaging.signals  # Ensures signals are registered
+        import messaging.signals
